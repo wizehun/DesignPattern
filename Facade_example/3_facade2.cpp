7e98c475ad8cdd62fc3b5d24c7569efc9b07e493
wizehun <90188068+wizehun@users.noreply.github.com>
2024-10-30 14:40:07 +0900

    Add files via upload

---
+++ Facade_example/3_facade2.cpp
@@ -0,0 +1,81 @@
+﻿﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+
+// SRP ( Single Responsibility Principle )
+// => 하나의 클래스는 하나의 책임만 있어야 한다.
+
+class NetworkInit
+{
+public:
+    NetworkInit()
+    {
+        // 1. 네트워크 라이브러리 초기화
+        WSADATA w;
+        WSAStartup(0x202, &w);
+    }
+    ~NetworkInit()
+    {
+        // 6. socket 라이브러리 cleanup
+        WSACleanup();
+    }
+};
+
+// IP 주소를 관리하는 클래스
+class IPAddress
+{
+    SOCKADDR_IN addr;
+public:
+    IPAddress(const char* ip, short port)
+    {
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        addr.sin_addr.s_addr = inet_addr(ip);
+    }
+    SOCKADDR* getRawAddress()
+    {
+        return (SOCKADDR*)&addr;
+    }
+};
+
+// Socket 작업을 책임지는 클래스
+class Socket
+{
+    int sock;
+public:
+    Socket(int type) { sock = socket(PF_INET, type, 0); }
+
+    void Bind(IPAddress* ip)
+    {
+        ::bind(sock, ip->getRawAddress(), sizeof(SOCKADDR_IN));
+    }
+    void Listen() { ::listen(sock, 5); }
+
+    void Accept()
+    {
+        struct sockaddr_in addr2 = { 0 };
+        int sz = sizeof(addr2);
+
+        accept(sock, (SOCKADDR*)&addr2, &sz);
+    }
+};
+
+// 라이브러리 설계자들이 위처럼 만들어 주면
+// 최종사용자는 아래 처럼 사용합니다.
+// facade1.cpp 보다는 아주 간결합니다.
+// => 객체지향의 장점
+
+int main()
+{
+    NetworkInit init;
+
+    Socket sock(SOCK_STREAM); // TCP 서버
+
+    IPAddress addr("127.0.0.1", 4000);
+    sock.Bind(&addr);
+    sock.Listen();
+    sock.Accept();
+
+}
---
+++ Facade_example/3_facade3.cpp
@@ -0,0 +1,100 @@
+﻿﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+
+
+class NetworkInit
+{
+public:
+    NetworkInit()
+    {
+
+        WSADATA w;
+        WSAStartup(0x202, &w);
+    }
+    ~NetworkInit()
+    {
+
+        WSACleanup();
+    }
+};
+
+class IPAddress
+{
+    SOCKADDR_IN addr;
+public:
+    IPAddress(const char* ip, short port)
+    {
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        addr.sin_addr.s_addr = inet_addr(ip);
+    }
+    SOCKADDR* getRawAddress()
+    {
+        return (SOCKADDR*)&addr;
+    }
+};
+
+class Socket
+{
+    int sock;
+public:
+    Socket(int type) { sock = socket(PF_INET, type, 0); }
+
+    void Bind(IPAddress* ip)
+    {
+        ::bind(sock, ip->getRawAddress(), sizeof(SOCKADDR_IN));
+    }
+    void Listen() { ::listen(sock, 5); }
+
+    void Accept()
+    {
+        struct sockaddr_in addr2 = { 0 };
+        int sz = sizeof(addr2);
+
+        accept(sock, (SOCKADDR*)&addr2, &sz);
+    }
+};
+
+// TCP Server 를 만들려면 
+// 1. 몇개의 클래스를 사용해서
+// 2. 일련의 절차를 수행해야 합니다.
+// => 이런 작업도 "하나의 책임(TCPServer라는 책임)" 입니다.
+
+// 이런 과정을 단순화 해서 "하나의 인터페이스(함수)로 제공" 하는 클래스를 만들어라
+// => "facade" 패턴
+
+class TCPServer
+{
+    NetworkInit init;
+    //	Socket sock(SOCK_STREAM); // 멤버 데이타에서는 () 초기화하면 에러
+    Socket sock{ SOCK_STREAM }; // {} 로 해야 합니다.
+public:
+    void Start(const char* ip, short port)
+    {
+        IPAddress addr(ip, port);
+        sock.Bind(&addr);
+        sock.Listen();
+        sock.Accept();
+    }
+};
+int main()
+{
+    // 최종 사용자 코드
+    TCPServer server;
+    server.Start("127.0.0.1", 4000);
+}
+
+// C++ MFC 라이브러리
+// => 마이크로소프트가 만든 GUI 라이브러리
+// => facade2.cpp 처럼만 되어 있음.
+// => 사용하기 어렵고 복잡하기로 악명 높음..
+
+
+// C#, Python
+// => facade3.cpp 처럼
+// => "TCPserver", "UDPServer" 같은 퍼사드 클래스가 아주 잘되어 있음.
+// => Python 의 pyexcel 등...
+// => 그래서, 사용하기 아주 쉽습니다.
