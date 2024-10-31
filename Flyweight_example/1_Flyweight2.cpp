5a94758f6671168f175e857631c08007e02dfa21
wizehun <90188068+wizehun@users.noreply.github.com>
2024-10-31 09:45:38 +0900

    Add files via upload

---
+++ Flyweight_example/1_Flyweight2.cpp
@@ -0,0 +1,68 @@
+﻿// 1_Flyweight - 167
+#include <iostream>
+#include <string>
+#include <map>
+
+class Counter {
+public:
+    static int count;  // 클래스 내에서 static 멤버 변수 선언
+
+    Counter() { ++count; }
+
+    static int getCount() {  // static 멤버 함수
+        return count;
+    }
+};
+int Counter::count = 0; // 클래스 외부에서 static 멤버 변수 정의
+
+class Image
+{
+    std::string image_url;
+    
+    // 생성자를 private에 둬서 외부에서 new로 못 만들게 한다.
+    Image(const std::string& url) : image_url(url)
+    {
+        std::cout << url << " Downloading..." << std::endl;
+    }
+
+public:
+    void Draw() { std::cout << "Draw " << image_url << std::endl; }
+
+
+    // inline static 문법
+    // => static 멤버 데이타를 만들때 클래스 외부 선언이 필요 없습니다
+    // => C++17 부터 지원되는 문법
+    static std::map<std::string, Image*> image_map;
+
+    // Image 객체를 만드는 static 멤버 함수
+    static Image* Create(const std::string& url)
+    {
+        // #1. 이미 만든 객체가 있는지 조사
+        auto it = image_map.find(url);
+
+        // #2. 없는 경우만 생성
+        if (it == image_map.end())
+        {
+            Image* img = new Image(url);
+            image_map[url] = img;
+            return img;
+        }
+        return it->second;
+    }
+};
+
+
+
+int main()
+{
+    // IOS 개발시 사용되는 "Cocoa Touch" 라이브러리의
+    // "UIImage"클래스가 아래처럼 사용합니다.(Swift 언어)
+
+    Image* img1 = Image::Create("www.naver.com/a.png");
+    img1->Draw();
+
+    Image* img2 = Image::Create("www.naver.com/a.png");
+    img2->Draw();
+
+}
+
---
+++ Flyweight_example/1_Flyweight4.cpp
@@ -0,0 +1,43 @@
+// godbolt.org
+#include <iostream>
+
+int main()
+{
+    // #1.
+//	printf("hello\n");	// 1. 상수메모리에 "hello" 문자열 만들고
+                        // 2. mov  edi, "상수메모리의 문자열주소"
+                        // 3. call printf
+
+
+    // #2. 문자열 배열 vs 문자열 포인터
+    char sa[] = "abcd";
+    //	char* sp  = "abcd"; // C ok. C++ error
+
+    *sa = 'x'; // ok. stack 에 쓰기, R/W 가능
+    //	*sp = 'x'; // C 언어. runtime error
+
+    const char* sp = "abcd"; // C++ ok
+    // 즉, 반드시 const 필요
+
+
+// #3. flyweight
+    char sa1[] = "abcd";
+    char sa2[] = "abcd";
+
+    const char* sp1 = "abcd";
+    const char* sp2 = "abcd";
+
+    printf("%p, %p\n", sa1, sa2); // 다른 주소
+    printf("%p, %p\n", sp1, sp2); // 같은 주소(공유)
+}
+
+// 프로그램에서 읽기만 하는 문자열이 필요하면
+// 문자열 배열이 아닌 문자열 포인터 사용
+
+// C++ 
+// std::string : 문자열 소유(동적할당해서 보관)
+// std::string_view : const char* 와 유사한 역활 (문자열 포인터)
+
+// Rust
+// str 타입 : const char*
+// String  : std::string 과 유사. 
